0f975a9f02a5f8a54c9e72a11d6ddae099abf9eb
Olasunkanmi Idris <olasunkanmiidris15@gmail.com>
2024-06-13 16:56:07 +0100

    served readme as home and finalized routes setup

--- app.ts
+++ app.ts
@@ -2,16 +2,45 @@ require("module-alias/register");
 require("dotenv").config();
 import connectDB from "@config/dbConfig";
 import envConfig from "@config/envConfig";
-import express from "express";
+import accoladesRouter from "@routes/accoladesRoutes";
+import certificationsRouter from "@routes/certificationsRoutes";
+import educationRouter from "@routes/educationRoutes";
+import projectsRouter from "@routes/projectsRoutes";
+import userRouter from "@routes/userRoutes";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { marked } from "marked";
 import mongoose from "mongoose";
-
-const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setting the Readme file as default screen for the app
+app.get("/", (req: Request, res: Response) => {
+  const readmePath = path.join(__dirname, "README.md");
+
+  fs.readFile(readmePath, "utf8", (err: any, data: any) => {
+    if (err) {
+      res.status(500).send("Error reading README file");
+      return;
+    }
+    const htmlContent = marked.parse(data);
+    res.send(htmlContent);
+  });
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/certification", certificationsRouter);
+app.use("/api/accolades", accoladesRouter);
+app.use("/api/education", educationRouter);
+app.use("/api/projects", projectsRouter);
 
 mongoose.connection.once("open", () => {
   console.log("Database Connection Successful");
